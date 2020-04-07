a88b84b7f717c01fe45654fcd2160e6fc93acc7c
xteal <32860377+NoNoTeal@users.noreply.github.com>
2020-04-06 16:55:35 -1000

    1.2.0

--- README.md
+++ README.md
@@ -2,6 +2,15 @@ Please refer to [the original](https://npmjs.com/package/node-merge) package's R
 
 # Includes
 
-• A [fix](https://github.com/steerapi/node-merge/pull/2) for "undefined merge"
+A [fix](https://github.com/steerapi/node-merge/pull/2) for "undefined merge"
+Fixes [path.existsSync](https://github.com/steerapi/node-merge/commit/f4ea6b3237eb88ac6ae594e9e23874849715900d)
 
-• Fixes [path.existsSync](https://github.com/steerapi/node-merge/commit/f4ea6b3237eb88ac6ae594e9e23874849715900d)
+## New feature(s)
+
+### 1.1.2
+
+Adds a silent option so your console is not spammed with results. Will also remove entries from `_results`.
+
+### 1.2.0 
+
+Added a deleteFile option. Option will do this: When merging a folder with another folder, if folder A contains files folder B doesn't have, instead of being merged, the folder is more or less being replaced. 
--- merge.js
+++ merge.js
@@ -3,13 +3,14 @@ var fs      = require("node-fs"),
 
 var defaultOptions  = {
   recursive: true,  // whether to merge recursively
-  overwrite: false  // whether to overwrite existing files
+  overwrite: false,  // whether to overwrite existing files
+  silent: true, //whether to console.log every entry
+  deleteFiles: false // if file is located in dir 2, but not in dir 1, it is deleted.
 };
 
 var mergeTo = function(f1, f2, options) {
   // store the options
   options = extend({}, defaultOptions, options);
-
   var file, files, stats, _i, _len, _results;
   files = fs.readdirSync(f1);
   _results = [];
@@ -18,7 +19,6 @@ var mergeTo = function(f1, f2, options) {
   for (_i = 0, _len = files.length; _i < _len; _i++) {
     file = files[_i];
     stats = fs.lstatSync(f1 + "/" + file);
-
     if (stats.isDirectory()) {
       // file is a directory
 
@@ -27,7 +27,9 @@ var mergeTo = function(f1, f2, options) {
         _results.push(mergeTo(f1 + "/" + file, f2 + "/" + file, options));
       }else{
         // not recursive - skip the directory
+        if(options.silent == false) {
         _results.push(console.log('Skipping directory ' + f2 + '/' + file));
+        }
       }
     } else {
       // normal file
@@ -35,10 +37,21 @@ var mergeTo = function(f1, f2, options) {
         // file doesn't exist or we can overwrite
         fs.mkdirSync((f2 + "/" + file).split("/").slice(0, -1).join("/"), 0x1ed, true);
         fs.writeFileSync(f2 + "/" + file, fs.readFileSync("" + f1 + "/" + file));
+        if(options.silent == false) {
         _results.push(console.log("Merged " + f2 + "/" + file));
+        } else
+      if (options.deleteFiles) {
+        if(!fs.existsSync(f1 + "/" + fs.readdirSync(f2.split('//').join('/'))[_i])) {
+        fs.unlinkSync((f2.split('//').join('/') + "/" + fs.readdirSync(f2.split('//').join('/'))[_i]).split('//').join('/'))
+        if(options.silent == false) {
+        _results.push(console.log("Overwrote & Deleted" + (f2.split('//').join('/') + "/" + fs.readdirSync(f2.split('//').join('/'))[_i]).split('//').join('/')))
+        }}
+      }
       } else {
         // file already exists
+        if(options.silent == false) {
         _results.push(console.log("File exists " + f2 + "/" + file));
+        }
       }
     }
   }
